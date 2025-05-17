0706a66898cceb2a9563707da3b9c9fd3b482c4d
LAM0578 <1805096805@qq.com>
2025-05-18 05:58:06 +0800

    202505180558

--- aa/scripts/scroll.js
+++ aa/scripts/scroll.js
@@ -1,9 +1,22 @@
 let currentIndex = 0;
+let scale = 1;
 const images = document.querySelectorAll('.image-container img');
 const imageContainer = document.querySelector('.image-container');
+const isFireFox = navigator.userAgent.toLowerCase().includes('firefox');
 
-function showImage(index) {
-    imageContainer.style.transform = `translateX(${-index * 100}%)`;
+function showImage(index, isRefresh) {
+    if (isFireFox) {
+        if (isRefresh) {
+            imageContainer.style.transition = 'none';
+        }
+        else {
+            imageContainer.style = '';
+        }
+        imageContainer.style.transform = `translateX(${-532 * index * scale}px)`;
+    }
+    else {
+        imageContainer.style.transform = `translateX(${-index * 100}%)`;
+    }
     images.forEach((img, i) => {
         img.classList.toggle('active', i === index);
     });
@@ -11,12 +24,25 @@ function showImage(index) {
 
 function nextImage() {
     currentIndex = (currentIndex + 1) % images.length;
-    showImage(currentIndex);
+    showImage(currentIndex, false);
 }
 
 function prevImage() {
     currentIndex = (currentIndex - 1 + images.length) % images.length;
-    showImage(currentIndex);
+    showImage(currentIndex, false);
+}
+
+function resize() {
+    scale = Math.min(Math.max(window.innerWidth / 532, 0.667), 1);
+    images.forEach((img, i) => {
+        img.style.width = `${532 * scale}px`;
+    })
+    showImage(currentIndex, true);
 }
 
 showImage(currentIndex);
+
+if (isFireFox) {
+    resize();
+    window.addEventListener('resize', resize);
+}
